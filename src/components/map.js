4a5c9b10dadc426b624cc59a48cae24dfa32fea5
Felipe Bobadilla <bobadilla91@gmail.com>
2017-06-13 23:16:55 -0400

    added filter to map

--- src/components/map.js
+++ src/components/map.js
@@ -4,6 +4,13 @@ import GoogleMapReact from 'google-map-react';
 import Navbar from './mapComponents/navbar';
 import TableView from './mapComponents/tableview';
 import mapStyle from './mapComponents/mapStyle.json';
+import mapDataExcel from './mapComponents/mapData.json';
+import icon1 from './media/3especialidades.png';
+import icon2 from './media/2especialidades.png';
+import icon3 from './media/1especialidad.png';
+import icon4 from './media/4especialidades.png';
+import icon5 from './media/5especialidades.png';
+import icon6 from './media/6especialidades.png';
 
 export default class Map extends Component {
 
@@ -12,13 +19,37 @@ export default class Map extends Component {
     this.state = {
       lat: -33.4367233,
       lng: -70.6524783,
-      zoom: 15,
+      zoom: 8,
+      types: [icon1, icon2, icon3, icon4, icon5, icon6],
+      tasa: '',
+      sector: '',
+      mapData: mapDataExcel,
     };
   }
 
+  filter() {
+    const { tasa, sector } = this.state;
+    let mapData = mapDataExcel;
+    if (tasa !== null && tasa.value !== '') {
+      mapData = mapData.filter((data) => {
+        if (tasa.value === 0) return data.tasaTitulacion <= 0.6;
+        else if (tasa.value === 1) return data.tasaTitulacion > 0.6 && data.tasaTitulacion < 0.8;
+        else if (tasa.value === 2) return data.tasaTitulacion >= 0.8;
+        else return true;
+      });
+    } if (sector.length > 0) {
+      mapData = mapData.filter((data) => {
+        let val = false;
+        data.sectorEconomico.split(',').forEach(sectorData => sector.forEach((sectorFilter) => { if (sectorFilter.label === sectorData) val = true; }));
+        return val;
+      });
+    }
+    return mapData;
+  }
 
   render() {
-    const { lat, lng, zoom } = this.state;
+    const { lat, lng, zoom, types, sector, tasa } = this.state;
+    const mapData = this.filter();
     return (
       <div>
         <GoogleMapReact
@@ -26,8 +57,12 @@ export default class Map extends Component {
           defaultZoom={zoom}
           style={{ display: 'flex' }}
           options={{ styles: mapStyle }}
-        />
-        <Navbar />
+        >
+          {mapData.map(data =>
+            <img key={data.id} alt="" src={types[data.tipo - 1]} style={{ height: 30, width: 30, position: 'absolute' }} lat={data.latLong.split(',')[0]} lng={data.latLong.split(',')[1]} />,
+        )}
+        </GoogleMapReact>
+        <Navbar changeSector={sectorValue => this.setState({ sector: sectorValue })} changeTasa={tasaValue => this.setState({ tasa: tasaValue })} sector={sector} tasa={tasa} />
         <TableView />
       </div>
     );
--- src/components/mapComponents/navbar.js
+++ src/components/mapComponents/navbar.js
@@ -12,39 +12,40 @@ export default class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sector: [
-        { label: 'Administración y comercio', value: 'adco' },
+      sectorLabeL: [
+        { label: 'Administración y Comercio', value: 'adco' },
         { label: 'Agropecuario', value: 'agpo' },
         { label: 'Alimentación', value: 'alim' },
         { label: 'Confección', value: 'conf' },
         { label: 'Construcción', value: 'cons' },
         { label: 'Electricidad', value: 'elec' },
         { label: 'Gráfica', value: 'graf' },
-        { label: 'Hotelera y Turismo', value: 'hotu' },
+        { label: 'Hotelería y Turismo', value: 'hotu' },
         { label: 'Maderero', value: 'made' },
         { label: 'Metalmecánico', value: 'meta' },
         { label: 'Minero', value: 'mine' },
         { label: 'Programas y Proyectos Sociales', value: 'pyps' },
         { label: 'Químico', value: 'quim' },
       ],
-      tasa: [
-        { label: '< 60%', value: '<60%' },
-        { label: '60% - 80%', value: '6080' },
-        { label: '> 80%', value: '>80%' },
-        { label: 'Sin Información', value: 'nn' },
+      tasaLabel: [
+        { label: '< 60%', value: 0 },
+        { label: '60% - 80%', value: 1 },
+        { label: '> 80%', value: 2 },
+        { label: 'Sin Información', value: 3 },
       ],
     };
   }
 
 
   render() {
-    const { sector, tasa } = this.state;
+    const { sectorLabeL, tasaLabel } = this.state;
+    const { sector, tasa } = this.props;
     return (
       <div>
-        <Col xs={12} md={10} mdOffset={1} style={{ marginTop: 80, backgroundColor: '#454561', paddingTop: 4, paddingBottom: 4 }}>
+        <Col className="animated fadeInDown" xs={12} md={10} mdOffset={1} style={{ marginTop: 80, backgroundColor: '#454561', paddingTop: 4, paddingBottom: 4 }}>
           <Col md={4} mdOffset={1}>
             <Col xs={10}>
-              <Select placeholder="Sector Económico" options={sector} multi />
+              <Select value={sector} placeholder="Sector Económico" options={sectorLabeL} multi onChange={a => this.props.changeSector(a)} />
             </Col>
             <Col xs={2}>
               <img src={imgSector} style={{ height: 32, margintop: 5 }} alt="lala" />
@@ -52,7 +53,7 @@ export default class Navbar extends Component {
           </Col>
           <Col md={3}>
             <Col xs={10}>
-              <Select placeholder="Tasa de Titulación" options={tasa} />
+              <Select value={tasa} placeholder="Tasa de Titulación" options={tasaLabel} onChange={this.props.changeTasa} />
             </Col>
             <Col xs={2}>
               <img src={imgTasa} style={{ height: 32, margintop: 5 }} alt="lala" />
